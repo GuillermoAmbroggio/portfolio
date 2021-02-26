c78c4fc5103dfbd75d8cc7b200b5da82b34d1fa4
GuillermoAmbroggio <guillermoambroggio@gmail.com>
2021-02-26 10:10:47 -0300

    :rocket: atoms implemented

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import useStore from "./hooks/useStore";
 import { Text, Title, SubTitle } from "./components/atoms/text/Text";
 import useTheme from "./hooks/useTheme";
 import Icon from "./components/atoms/icon/Icon";
+import ProgressBar from "./components/atoms/progressBar/ProgressBar";
 
 const App: React.FC = () => {
   const { isLoading } = useStore();
@@ -19,10 +20,12 @@ const App: React.FC = () => {
       <Text style={{ backgroundColor: theme.palette.primary }}>Regular</Text>
       <Title>Title</Title>
       <SubTitle>Sub Title</SubTitle>
+      <ProgressBar status="50%" />
       <p style={{ background: "blue", color: "green", fontSize: "150px" }}>
         <span className="fab fa-instagram" style={{ color: "red" }} />
       </p>
       <Icon name="fab fa-instagram" style={{ fontSize: 66 }} />
+      <p>asdasdds</p>
     </div>
   );
 };
