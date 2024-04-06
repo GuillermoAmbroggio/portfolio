2a5109e8dcb9047c8f9a0e1b15f63056c048c155
guillermo <guillermoambroggio@gmail.com>
2024-04-06 17:40:50 -0300

    :art: cv update

--- src/components/molecules/downloadCV/DownloadCV.tsx
+++ src/components/molecules/downloadCV/DownloadCV.tsx
@@ -1,17 +1,25 @@
-import React from "react";
-import CV from "./pdf/CVGuillermoAmbroggio.pdf";
-import Icon from "../../atoms/icon/Icon";
-import { Link, ContainerDownload, TextCV } from "./downloadcv.style";
-import Writing from "../../../copywriting/Writing";
+import React from 'react';
+import CV from './pdf/CVGuillermoAmbroggio.pdf';
+import CVI from './pdf/CVGuillermoAmbroggioI.pdf';
+
+import Icon from '../../atoms/icon/Icon';
+import { Link, ContainerDownload, TextCV } from './downloadcv.style';
+import Writing from '../../../copywriting/Writing';
+import useStore from '../../../hooks/useStore';
 
 const DownloadCV: React.FC = () => {
   const texts = Writing();
+  const { language } = useStore();
+
   return (
-    <Link href={CV} download="CVGuillermoAmbroggio.pdf">
+    <Link
+      href={language === 'en' ? CVI : CV}
+      download='CVGuillermoAmbroggio.pdf'
+    >
       <ContainerDownload>
-        <Icon name="fas fa-file-download" size={30} />
+        <Icon name='fas fa-file-download' size={30} />
         <TextCV>CV</TextCV>
-        <span className="textMenu">{texts.download}</span>
+        <span className='textMenu'>{texts.download}</span>
       </ContainerDownload>
     </Link>
   );
